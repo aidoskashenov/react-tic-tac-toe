623b8e1788cb917ba204a1ce63ffc43ce3e7ad74
Manav Misra <manavm@visionwebsoft.com>
2020-05-28 17:29:33 -0500

    :seedling: Add starter components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,17 @@
 import React from "react";
 import "./App.css";
 
-export const App = () => <div className="App"></div>;
+import { Square } from "./components";
+
+export class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="status"></div>
+        <div className="board-row"></div>
+        <div className="board-row"></div>
+        <div className="board-row"></div>
+      </div>
+    );
+  }
+}
